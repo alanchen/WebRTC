58c853d7ee6318c406c4f807c5e16ed4e3169249
Alan <mvpi3.tw@gmail.com>
2018-09-18 11:02:21 +0800

    Initial Commit

---
+++ BeFluent/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeFluent
+//
+//  Created by alan on 2018/9/18.
+//  Copyright © 2018年 alan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BeFluent/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BeFluent
+//
+//  Created by alan on 2018/9/18.
+//  Copyright © 2018年 alan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
