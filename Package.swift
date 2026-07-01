57cb211c42e95a6d890e2d62ef25a5326e196c5a
Anka <runner@sat12-bq154-f42703bf-c4e4-450f-9862-a464ae08a314-DA5E4568BDC9.local>
2026-07-01 15:58:03 +0000

    Updated files for release M150

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/alanchen/WebRTC/releases/download/144.0.0/WebRTC-M144.xcframework.zip",
-            checksum: "95615822722e31b9715e1959a83475b501f15928a3a8fb284f261ba4eabf1b8f"
+            url: "https://github.com/alanchen/WebRTC/releases/download/150.0.0/WebRTC-M150.xcframework.zip",
+            checksum: "399b0d744d66d1653cad87a5056d6c4c47fa2447decd769d2efc3e84ab60252c"
         ),
     ]
 )
